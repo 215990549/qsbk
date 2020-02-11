7574be8f0b787341827f022aa7053bffdffd6765
joe <215990549@qq.com>
2020-02-11 23:29:40 +0800

    引入官方CSS、矢量图、动画库

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 官方CSS文件 */
+	@import './common/uni.css';
+	/* 矢量图库 */
+	@import './common/icon.css';
+	/* 动画库 */
+	@import './common/animate.css';
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<span>矢量图片测试</span> 
+		<button type="primary"@click='animate'>动画测试</button>
+		<view class="icon iconfont icon-pic animated bounceIn" v-show="animateFlag"></view>
+		
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				animateFlag:false
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			animate () {
+				this.animateFlag = !this.animateFlag
+				
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	
+	.icon-pic{
+		font-size: 120rpx;
+	}
+</style>
